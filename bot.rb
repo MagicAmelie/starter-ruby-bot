c4ecf7fc2340d3358b727c0715a35d7288a753c2
MagicAmelie <amelie.stephan@t-online.de>
2016-03-19 14:19:20 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -49,7 +49,7 @@ client.on :message do |data|
     logger.debug("Attachment message posted")
 
   when bot_mentioned(client)
-    client.message channel: data['channel'], text: 'You really do care about me. :heart:'
+    client.message channel: data['channel'], text: 'Hallo! Schön mit dir zu chatten :+1:'
     logger.debug("Bot mentioned in channel #{data['channel']}")
 
   when 'bot help', 'help' then
