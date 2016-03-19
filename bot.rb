ad8c9f367fda8b768f7638f7ee11b853c905f02f
MagicAmelie <amelie.stephan@t-online.de>
2016-03-19 14:57:32 +0100

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -71,7 +71,8 @@ client.on :message do |data|
     logger.debug("Unknown command")
     
   when 'Wie ist das Wetter?' then
-    Net:HTTP.get('http://api.openweathermap.org', '/data/2.5/weather?q=Bonn&appid=b1b15e88fa797225412429c1c50c122a')
+    wetterinfo =Net:HTTP.get('http://api.openweathermap.org', '/data/2.5/weather?q=Bonn&appid=b1b15e88fa797225412429c1c50c122a')
+    client.message channel: data['channel'], text: wetterinfo
   end
 end
 
